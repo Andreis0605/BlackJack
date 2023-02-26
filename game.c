e496248b26b3e8002c8ad131926375d6ce83dc19
Andreis0605 <andreis0605@gmail.com>
2023-02-27 00:29:42 +0200

    Changes to be committed: 	modified:   game.c 	modified:   player.c

--- game.c
+++ game.c
@@ -7,6 +7,5 @@
 
 int main()
 {
-    f();
     return 0;
 }
--- player.c
+++ player.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "deck_operations.h"
+
+void player_add_in_hand(char *player_hand, char card)
+{
+    int n;
+    n = strlen(player_hand);
+    player_hand[n] = card;
+    player_hand[n + 1] = '\n';
+}
+
+int hand_value(char *player_hand)
+{
+    int value;
+}
