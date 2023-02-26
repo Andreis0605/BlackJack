51884e5ca6efee61ca88467d76b508d805a08a1d
Andreis0605 <andreis0605@gmail.com>
2023-02-27 00:26:54 +0200

    start of the player library

--- game.c
+++ game.c
@@ -3,14 +3,10 @@
 #include <time.h>
 #include "deck_operations.h"
 #include "interface.h"
+#include "player.h"
 
 int main()
 {
-    int k, deck[53], i;
-    char card;
-    initialize_deck(deck);
-    k=draw_card(deck);
-    card=card_translation(k);
-    printf("%c\n",card);
+    f();
     return 0;
 }
---
+++ player.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "deck_operations.h"
